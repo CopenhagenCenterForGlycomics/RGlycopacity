0983840898b54932a1f100b5a3f046b2fa11a273
Hiren Joshi <hirenj@gmail.com>
2022-01-12 23:56:57 +0100

    Add in better looking faceting based on glycogenome

--- R/facet_glycogenome.R
+++ R/facet_glycogenome.R
@@ -1,6 +1,16 @@
 
 test <- function() {
-  ggplot(data.frame(gene=c('GALNT2','GALNT10','POFUT1','POFUT2'),baz=rep(c('a','b'),4),x=1:4),aes(x,y=gene))+geom_point()+facet_glycogenome(genes=vars(gene))
+  allgenes = unique(glycogenome_ordering$HGNC)
+  allgenes = sample(allgenes,30)
+  #ggplot(data.frame(gene=allgenes,baz=sample(c('a','b'),length(allgenes),T),x=rnorm(length(allgenes),10,0.1)),aes(x,y=gene))+geom_point()+facet_glycogenome(genes=vars(gene))
+  ggplot(data.frame(gene=rep(allgenes,3),baz=sample(c('a','b'),3*length(allgenes),T),x=rnorm(3*length(allgenes),10,0.1)),aes(x))+geom_histogram()+facet_glycogenome(genes=vars(gene))
+}
+
+rle_strings = function(strings) {
+  rl <- rle(strings)
+  lst <- lapply(split(seq_along(strings), rep(seq_along(rl$values), rl$lengths)), range)
+  names(lst) <- rl$values
+  do.call(rbind, lst)
 }
 
 if (requireNamespace("ggplot2",quietly=TRUE)) {
@@ -53,17 +63,40 @@ FacetGlycogenome <- ggplot2::ggproto("FacetGlycogenome", ggplot2::FacetGrid,
   },
   draw_panels = function(panels, layout, x_scales, y_scales, ranges, coord, data, theme, params) {
     theme = theme + theme(panel.spacing.x = unit(0.1,"lines"),
-         panel.spacing.y = unit(1,"lines"),
+         panel.spacing.y = unit(0.1,"lines"),
          strip.background.x = element_rect(size=1),
          strip.placement = "outside",
          strip.text.x = element_text(color = "black", face = "bold"),
          strip.background.y = element_blank(),
-         strip.text.y = element_text(),
          axis.title.x = element_blank(),
          axis.title.y = element_blank()
     )
+
     params$rows = list(glycogene_pathway=quo(glycogene_pathway))
-    return(ggplot2::FacetGrid$draw_panels(panels, layout, x_scales, y_scales, ranges, coord, data, theme, params))
+
+    drawn = ggplot2::FacetGrid$draw_panels(panels, layout, x_scales, y_scales, ranges, coord, data, theme, params)
+    label_widths = lapply(drawn$grobs[which(grepl('axis-l',drawn$layout$name))], grid::grobWidth)
+
+    used_pathways = unique(layout[,c('ROW','glycogene_pathway')])[,2]
+
+    pathways_indices = rle_strings(stringr::str_replace(as.character(layout$glycogene_pathway),":.*",""))
+
+
+
+    drawn$grobs[grepl('strip-l',drawn$layout$name)] = lapply(used_pathways, function(x) generate_pathway_element(x,extra_width=label_widths[[1]]))
+    drawn$layout[grepl('strip-l',drawn$layout$name),]$clip = 'off'
+
+    drawn = gtable::gtable_add_cols(drawn, unit(1, "cm"), 0)
+
+    apply(cbind(pathways_indices,rownames(pathways_indices)), 1, function(run) {
+      label = grid::textGrob(run[3],rot=90)
+      line = grid::linesGrob(x = unit(c(0.75, 0.75), "npc"),
+          y = unit(c(0.01, 0.99), "npc"),gp = grid::gpar(color = "#00000000",lwd=2))
+      title = grid::grobTree(label,line)
+      drawn <<- gtable::gtable_add_grob(drawn,grobs = title,t =as.numeric(run[1])*2,b=as.numeric(run[2])*2,l=1,r=1)
+    })
+
+    return(drawn)
   }
 
 )
---
+++ R/generate_pathway_element.R
@@ -0,0 +1,44 @@
+
+generate_pathway_element <- function(label,extra_width=grid::unit(0,"npc")) {
+
+  colours = read.delim('data/pathway_colours.tsv',header=T,comment.char='',na.strings=c(''))
+  bar_lookup = with(colours,setNames(bar,pathway))
+  shade_lookup = with(colours,setNames(shade,pathway))
+  shape_lookup = with(colours,setNames(sugar,pathway))
+
+  pathway_class = stringr::str_replace(label,":.*","")
+  pathway = stringr::str_replace(label,".*:","")
+  if (is.na(pathway)) {
+    return(grid::nullGrob())
+  }
+
+  shade = NULL
+  if (pathway_class != "Initiation") {
+    shade = shade_lookup[pathway]
+  }
+  pathway_strip = grid::nullGrob()
+  if (!is.na(bar_lookup[pathway])) {
+    pathway_strip = grid::rectGrob(grid::unit(0,"npc"), grid::unit(0.5,"npc"), grid::unit(0.5,"npc"), grid::unit(1,"npc"), just=c(0,0.5), gp = grid::gpar(fill=bar_lookup[pathway],lwd=1), name="path_strip")
+  }
+
+  min_shade_width = unit(0.5,"cm")
+
+  pathway_shade = grid::rectGrob(grid::unit(0.5,"npc"), grid::unit(0.5,"npc"), min_shade_width + extra_width , grid::unit(1,"npc"), just=c(0,0.5), gp = grid::gpar(fill=shade,lwd=0), name="path_shade")
+
+  sugar_grob = grid::nullGrob()
+  if (requireNamespace('ggsugar',quietly=TRUE)) {
+    if (!is.na(shape_lookup[pathway])) {
+      sug = ggsugar:::get_template_sugar(shape_lookup[pathway])
+      sugar_viewport = grid::viewport(
+        x=grid::unit(0.5,"npc") + grid::unit(0,"native"),
+        y=grid::unit(0,"native"),
+        width=min_shade_width,
+        height=grid::unit(1,"npc"),
+        just=c(0,0)
+      )
+      sugar_grob = grid::gTree(vp=sugar_viewport,children = do.call(grid::gList,sug))
+    }
+  }
+
+  grid::grobTree(pathway_shade,pathway_strip,sugar_grob)
+}
